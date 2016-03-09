9e8b84d6a8e3688c769cab575cf5b731abf9cdc8
Xinlai He <Xinlai_He@epam.com>
2016-03-09 17:06:43 +0800

    modify landing page

---
+++ public/javascripts/main.js
@@ -0,0 +1,20 @@
+function showLoginPanel(){
+
+  $(".loginpanel").css("display", "block");
+  $("#welcome p").css("color", "transparent");
+}
+function hideLoginPanel(){
+
+  $(".loginpanel").css("display", "none");
+  $("#welcome p").css("color", "white");
+}
+function showRegisterPanel(){
+
+  $(".registerpanel").css("display", "block");
+  $("#welcome p").css("color", "transparent");
+}
+function hideRegisterPanel(){
+
+  $(".registerpanel").css("display", "none");
+  $("#welcome p").css("color", "white");
+}
